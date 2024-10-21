847af9e5ccb81ccda4af2a13a6af5440d9ed29e5
parham-devdocs <p.77.pazargadi@gmail.com>
2024-10-21 23:00:00 +0330

    tailwind config completed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path, { resolve } from "path";
+import tailwindCss from "tailwindcss";
 export default defineConfig({
   plugins: [react()],
-})
+  css:{postcss:{plugins:[tailwindCss]}},
+  resolve: {
+    alias: [{ find:"@",replacement:path.resolve(__dirname,"src") }],
+  },
+});
